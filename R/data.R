7e12ad7b5943935fa10b367a6fc7bf625568e475
thothal <foren@thothal.com>
2021-06-25 01:01:59 +0200

    Update data documentation to markdown format
    We switched to the new possibility of roxygen to write documentation
    using markdown instead of Rd syntax. In this commit we updated the
    docuemtation of the datasets to the new format.

--- R/data.R
+++ R/data.R
@@ -3,19 +3,22 @@
 #' A dataset containing all the colors and the imported Google font in the bulma
 #' stylesheet.
 #'
-#' @format A data frame with 70 rows and 4 variables:
+#' @format A data frame with `r nrow(bulma_config)` rows and `r ncol(bulma_config)`
+#' variables:
 #' \describe{
-#'   \item{theme}{character, the name of the theme (here this equals always
+#'   \item{theme}{\[`character(1)`\], the name of the theme (here this equals always
 #'      \sQuote{bulma} and is included to be inline with
-#'      \code{\link{bulmaswatch_config}}.)}
-#'   \item{group}{character, either \sQuote{color} or \sQuote{font} describing whether the
-#'   variable describes a color or the font.}
-#'   \item{variable}{character, the name of color or \sQuote{name} or \sQuote{id} for
-#'   fonts describing the name or the id.}
-#'   \item{value}{character, the hex color code or the name of the font used.}
+#'      [bulmaswatch_config].)}
+#'   \item{group}{\[`character(1)`\], either \sQuote{color} or \sQuote{font} describing
+#'      whether the variable describes a color or the font.}
+#'   \item{variable}{\[`character(1)`\], the name of color or \sQuote{name} or
+#'      \sQuote{id} for fonts describing the name or the id.}
+#'   \item{value}{\[`character(1)`\], the hex color code or the name of the font used.}
 #' }
-#' @seealso \code{\link{bulmaswatch_config}}
-#' @source \url{https://bulma.io/documentation/customize/variables/#derived-variables}
+#' @name bulma_config
+#' @seealso [bulmaswatch_config]
+#' @docType data
+#' @source <https://bulma.io/documentation/customize/variables/#derived-variables>
 "bulma_config"
 
 #' Colors and Font Used in the Bulmaswatch Themes
@@ -23,15 +26,18 @@
 #' A dataset containing all the colors and the imported Google font in the bulmaswatch
 #' themes.
 #'
-#' @format A data frame with 1476 rows and 4 variables:
+#' @format A data frame with `r nrow(bulmaswatch_config)` rows and
+#' `r ncol(bulmaswatch_config)` variables:
 #' \describe{
-#'   \item{theme}{character, the name of the theme.}
-#'   \item{group}{character, either \sQuote{color} or \sQuote{font} describing whether the
+#'   \item{theme}{\[`character(1)`\], the name of the theme.}
+#'   \item{group}{\[`character(1)`\], either \sQuote{color} or \sQuote{font} describing whether the
 #'   variable describes a color or the font.}
-#'   \item{variable}{character, the name of color or \sQuote{name} or \sQuote{id} for
+#'   \item{variable}{\[`character(1)`\], the name of color or \sQuote{name} or \sQuote{id} for
 #'   fonts describing the name or the id.}
-#'   \item{value}{character, the hex color code or the name of the font used.}
+#'   \item{value}{\[`character(1)`\], the hex color code or the name of the font used.}
 #' }
-#' @seealso \code{\link{bulma_config}}
-#' @source \url{https://jenil.github.io/bulmaswatch/}
+#' @name bulmaswatch_config
+#' @seealso [bulma_config]
+#' @docType data
+#' @source <https://jenil.github.io/bulmaswatch/>
 "bulmaswatch_config"
